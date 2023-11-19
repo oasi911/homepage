407855df98684d02f4d1edabf487f3f106fb2109
Anton <sokolanton.1998@gmail.com>
2023-11-19 16:33:01 +0200

    add about section adaptive design

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,48 @@
 import Main from "@/components/layout/Main";
-import { Flex, Heading, VStack, Text } from "@chakra-ui/layout";
+import { Flex, Heading, VStack, Text, Box } from "@chakra-ui/layout";
 import { Image } from "@chakra-ui/image";
 import Photo from "@/components/Photo";
 
 export default function Home() {
   return (
     <Main title="Homepage">
-      <Flex as="main">
-        <VStack as="section" spacing="25px" my="50px">
-          <Photo />
-          <Heading as="h1" size="2xl" textAlign="center">
-            Front-End React Developer
-            <Image
-              src="/waving-hand.png"
-              display="inline-block"
-              verticalAlign="middle"
-              boxSize="2.25rem"
-              ml="2"
-              alt="Waving hand"
-            ></Image>
-          </Heading>
-          <Text fontSize="2xl" w="100%" textAlign="justify">
-            Hi, I'm Anton Sokolchuk. A passionate Front-end React Developer
-            based in Ukraine, Dnipro. 📍
-          </Text>
-        </VStack>
-      </Flex>
+      <Box as="main">
+        <Flex
+          as="section"
+          direction={{ base: "column", lg: "row-reverse" }}
+          alignItems="center"
+        >
+          <Flex ml={{ base: "0", lg: "50px" }}>
+            <Photo />
+          </Flex>
+          <VStack
+            h={{ base: "100%", lg: "200px" }}
+            spacing={{ base: "25px", lg: "0" }}
+            justifyContent={{ base: "center", lg: "space-around" }}
+            my="50px"
+          >
+            <Heading
+              as="h1"
+              fontSize={{ base: "35px", md: "45px" }}
+              textAlign="center"
+            >
+              Front-End React Developer
+              <Image
+                src="/waving-hand.png"
+                display="inline-block"
+                verticalAlign="middle"
+                boxSize="1em"
+                ml="2"
+                alt="Waving hand"
+              />
+            </Heading>
+            <Text fontSize="2xl" w="100%" textAlign="justify">
+              Hi, I'm Anton Sokolchuk. A passionate Front-end React Developer
+              based in Ukraine, Dnipro. 📍
+            </Text>
+          </VStack>
+        </Flex>
+      </Box>
     </Main>
   );
 }
